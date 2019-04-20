0394e7e446b40d29993f25f3ec6295c6e9308af1
Camila <camilauachave@ua.pt>
2019-04-20 15:13:10 +0100

    chef

--- Chef.py
+++ Chef.py
@@ -8,11 +8,7 @@ import logging
 import argparse
 import threading
 import queue
-<<<<<<< HEAD
 import node from node 
-=======
-from node import Node 
->>>>>>> 0cf3229816cd61c2a70a44c0d44e9d91f8d7a716
  
 
 logging.basicConfig(level=logging.DEBUG,
@@ -23,41 +19,16 @@ logger = logging.getLogger('Chef')
 
 class Chef(threading.Thread):
     def __init__(self, id, address,name, successor_addr = None):
-        threading.Thread.__init__(self)
-        self.node = Node(id, address, name, successor_addr)
-<<<<<<< HEAD
-		self.id = id
-		self.adress = address
-		self.ring = ring
-		self.deliever_order = {}
-		self.recv_orders = {}
-		self.table={'RECEPCIONIST':1,'CHEF':None,'RESTAURANT':None,'WAITER':None}
+    threading.Thread.__init__(self)
+    self.node = Node(id, address, name, successor_addr)
+        self.id = id
+        self.adress = address
+        self.ring = ring
+        self.deliever_order = {}
+        self.recv_orders = {}
+        self.table={'RECEPCIONIST':1,'CHEF':None,'RESTAURANT':None,'WAITER':None}
 
     
-
-    def run(self):
-    	counter = False
-        self.node.start()
-		o = self.node.queuein()
-		t = self.node.table
-        v = o.get()
-        
-        if v['method'] == 'ORDER':
-        	self.recv_orders[v['args']['ticket']] = v['args']['order']
-        	for key in self.recv_orders[ticket]:	
-    	  		self.deliever_orders[ticket][key]=0
-        	if v['args']['order']['hamburguer'] is not None:
-        	   self.node.queueout({'id':t['RESTAURANT'],'method':'GRILL_HAMBURGER','args':'ticket':self.ticket}})
-        	elif v['args']['order']['drink'] is not None:
-            	 self.node.queueout({'id':t['RESTAURANT'],'method':'PREPARE_DRINK','args':'ticket':self.ticket}})
-        	elif v['args']['order']['friedPotato'] is not None:	
-        		 self.node.queueout({'id':t['RESTAURANT'],'method':'FRY_POTATO','args':'ticket':self.ticket}})
-        elif v['method'] == 'HAMBURGER_DONE':
-        	  self.deliever_orders[v['args']['ticket']]['hamburger']=self.deliever_orders[v['args']['ticket']]['hamburger']+1
-        	  counter = True
-=======
-        self.deliever_order = {}
-        self.recv_orders = {}          
 
     def run(self):
         counter = False
@@ -71,15 +42,14 @@ class Chef(threading.Thread):
             for key in self.recv_orders[ticket]:    
                 self.deliever_orders[ticket][key]=0
             if v['args']['order']['hamburguer'] is not None:
-               self.node.queueout({'id':t['RESTAURANT'],'method':'GRILL_HAMBURGER','args':{'ticket':self.ticket}})
+               self.node.queueout({'id':t['RESTAURANT'],'method':'GRILL_HAMBURGER','args':'ticket':self.ticket}})
             elif v['args']['order']['drink'] is not None:
-                 self.node.queueout({'id':t['RESTAURANT'],'method':'PREPARE_DRINK','args':{'ticket':self.ticket}})
+                 self.node.queueout({'id':t['RESTAURANT'],'method':'PREPARE_DRINK','args':'ticket':self.ticket}})
             elif v['args']['order']['friedPotato'] is not None: 
-                 self.node.queueout({'id':t['RESTAURANT'],'method':'FRY_POTATO','args':{'ticket':self.ticket}})
+                 self.node.queueout({'id':t['RESTAURANT'],'method':'FRY_POTATO','args':'ticket':self.ticket}})
         elif v['method'] == 'HAMBURGER_DONE':
               self.deliever_orders[v['args']['ticket']]['hamburger']=self.deliever_orders[v['args']['ticket']]['hamburger']+1
               counter = True
->>>>>>> 0cf3229816cd61c2a70a44c0d44e9d91f8d7a716
         elif v['method'] == 'DRINK_DONE':
               self.deliever_orders[v['args']['ticket']]['drink']=self.deliever_orders[v['args']['ticket']]['drink']+1
               counter = True
@@ -87,25 +57,10 @@ class Chef(threading.Thread):
               self.deliever_orders[v['args']['ticket']]['potato']=self.deliever_orders[v['args']['ticket']]['potato']+1
               counter = True
         
-<<<<<<< HEAD
         if counter = True :
-        	delivery = True
-	        for key in self.recv_orders[v['args']['ticket']]:
-	        	if not self.deliever_orders[v['args']['ticket']][key]==self.recv_orders[v['args']['ticket']][key]:
-	            	delivery = False
-	        self.node.queueout('id': t['WAITER'], method : 'FOOD_READY', 'args': ticket)       	
-
-
-
-
-
-        pass
-=======
-        if counter == True:
             delivery = True
             for key in self.recv_orders[v['args']['ticket']]:
                 if not self.deliever_orders[v['args']['ticket']][key]==self.recv_orders[v['args']['ticket']][key]:
                     delivery = False
-            self.node.queueout({'id': t['WAITER'], method : 'FOOD_READY', 'args':{ 'ticket': ticket}})        
->>>>>>> 0cf3229816cd61c2a70a44c0d44e9d91f8d7a716
+            self.node.queueout('id': t['WAITER'], method : 'FOOD_READY', 'args': ticket)        
 
